ab3a6d8630742a596665e393c1234c70f263bc68
Éric  António <ericantoniodev@gmail.com>
2024-07-30 22:57:42 +0200

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,6 +1,20 @@
 import styles, { layout } from "../style";
 import { features } from "../constants";
-import { Button } from "./index";
+import { Button, FeedBackCard } from "./index";
+
+const FeatureCard = ({icon , title , content ,index }) => {
+  return (
+    <div className={`flex flex-row p-6  rounded-[20px] ${index !== features.length -1 ?  "mb-6" : "mb-0" } feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+      </div>
+      <div className="flex-1 flex-col ml-3">
+        <h4 className="font-poppins font-normal text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+        <p className="font-poppins text-dimWhite  text-white text-[16px] leading-[23px] mb-1" >{content}</p>
+      </div>
+    </div>
+  )
+}
 
 const Business = () => {
   return (
@@ -16,6 +30,12 @@ const Business = () => {
           of credit cards on the market.
         </p>
         <Button styles="mt-10"/>
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col `}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {... feature} />
+        ))}
       </div>
     </section>
   );
