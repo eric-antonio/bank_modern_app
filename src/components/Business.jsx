bd22ca8336b7931d04df61f4ce4ce88f408edb7d
Éric  António <ericantoniodev@gmail.com>
2024-07-30 22:34:52 +0200

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -15,6 +15,7 @@ const Business = () => {
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </p>
+        <Button styles="mt-10"/>
       </div>
     </section>
   );
