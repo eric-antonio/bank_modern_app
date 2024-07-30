87f3d09b30401fa8edabf4e406ee10812b1a684c
Éric  António <ericantoniodev@gmail.com>
2024-07-30 22:34:55 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
 
-const Button = () => {
+
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
+    <button  type='button' className={`bg-blue-gradient px-6 py-3  font-medium font-poppins text-[18px]  outline-none rounded-lg ${styles}`}>
+      Get Started
+    </button>
   )
 }
 
