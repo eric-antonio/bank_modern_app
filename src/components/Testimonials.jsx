9d5e4d5d301b58f5f56bfeef94937b212d1441a1
Éric  António <ericantoniodev@gmail.com>
2024-07-31 00:18:03 +0200

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,12 +11,18 @@ const Testimonials = () => {
     >
       {/*! Todo */}
       <div />
-      <div className="w-full flex justify-between items-center md:flex-row flex-coll sm:mb-16 mb-6 relative ">
+      <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative ">
         <h1 className={`${styles.heading2}`}>What people are <br className="sm:block hidden" /> saying about us</h1>
 
         <div className="w-full md:mt-0 mt-6">
           <p className={`${styles.paragraph} text-left max-w-[450px]`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
         </div>
+      </div>
+
+      <div className={`flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]`}>
+        {feedback.map((card, index) => (
+          <FeedBackCard key={index} {...card} />
+        ))}
       </div>
     </section>
   );
