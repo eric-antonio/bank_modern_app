858e6c89c566857e60a3c86965cc21d4bf1dc80a
Éric  António <ericantoniodev@gmail.com>
2024-07-31 15:43:19 +0200

    Update Clients.jsx

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from '../style'; 
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}> 
+        {clients.map((client)=>(
+          <div className={`${styles.flexCenter} w-1/4 p-4`} key={client.id}>
+            <img src={client.logo} alt={client.name} className="sm:w-[192px] w-[100px] object-contain"/>
+          </div>
+        ))}</div>
+    </section>
   )
 }
 
