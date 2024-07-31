0ca4a3c58c6861be5a2cc7c9344b9bbff005289b
Éric  António <ericantoniodev@gmail.com>
2024-07-31 17:07:15 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,21 +14,15 @@ const Footer = () => {
             A new way to make the payments <br /> easy, reliable and secure.
           </p>
         </div>
-
-        <div
-          className={` flex flex-1 flex-row justify-between flex-wrap md:mt-0 mt-10`}
-        >
+        <div className="flex-[1.5] w-full flex  flex-row justify-between flex-wrap md:mt-0 mt-10">
           {footerLinks.map((footerLink) => (
-            <div
-              key={footerLink.key}
-              className={`${styles.flexCenter} flex flex-col  ss:my-0 my-4  min-w-[150px]`}
-            >
-              <h4 className="font-poppins font-medium  leading-[23px] text-[18px] text-white">
-                {footerLink.title}
-              </h4>
+            <div key={footerLink.key} className="flex flex-col ss:my-0  my-4 min-w-[150px] ">
+              <h4 className="text-white font-semibold ">{footerLink.title}</h4>
               <ul>
-                { footerLink.links.map((link) => (
-                  <li key={link.name} className={` font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>{link.name}</li>
+                {footerLink.links.map((link) => (
+                  <li key={link.key} className="text-gray-300 text-sm  cursor-pointer hover:text-secondary font-poppins my-2">
+                    {link.name}
+                  </li>
                 ))}
               </ul>
             </div>
