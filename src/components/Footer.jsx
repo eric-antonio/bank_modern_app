9e249f030353c9da29c6e1c93ecdca72f11bc019
Éric  António <ericantoniodev@gmail.com>
2024-07-31 16:46:59 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,50 @@
 import styles from "../style";
-import {logo} from "../assets";
-import { footerLinks , socialMedia} from "../constants"; 
+import { logo } from "../assets";
+import { footerLinks, navLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section
+      className={`${styles.flexCenter} ${styles.padding} ${styles.paddingY} flex-col`}
+    >
+      <div className={`${styles.flexStart} md:flex-row  flex-col mb-8 w-full`}>
+        <div className="flex-1 flex flex-col justify-start mr-10">
+          <img src={logo} alt="Hoobank" className="w-[266px] h-[72px]" />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]:`}>
+            A new way to make the payments <br /> easy, reliable and secure.
+          </p>
+        </div>
 
-export default Footer
+        <div
+          className={` flex flex-1 flex-row justify-between flex-wrap md:mt-0 mt-10`}
+        >
+          {footerLinks.map((footerLink) => (
+            <div
+              key={footerLink.key}
+              className={`${styles.flexCenter} flex flex-col  ss:my-0 my-4  min-w-[150px]`}
+            >
+              <h4 className="font-poppins font-medium  leading-[23px] text-[18px] text-white">
+                {footerLink.title}
+              </h4>
+              
+              <ul>
+                {footerLink.links.map((link) => (
+                  <li key={link.key}>
+                    <a
+                      className="font-poppins font-normal text-[16px] leading-[23px] text-dimWhite hover:text-secondary cursor-pointer"
+                      href={link.link}
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
